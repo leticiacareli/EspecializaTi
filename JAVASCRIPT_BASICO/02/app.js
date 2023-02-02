b891ba0d2b04ba7b8f9c674b46e816e2dfb9cba0
leticiacareli <leticia_careli@hotmail.com>
2023-02-02 08:33:47 -0300

    Aula 02
    Variáveis

---
+++ JAVASCRIPT_BASICO/02/app.js
@@ -0,0 +1,13 @@
+var nome = 'Letícia';
+console.log(nome);
+
+nome = 123;
+console.log(nome);
+
+var car = {
+    nome: "Fusion",
+    ano: 2018,
+    marca: 'Ford'
+};
+
+console.log(car.nome, car.marca);
