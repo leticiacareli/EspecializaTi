25a747b88ec5e50035d7bc484f210154d605a691
leticiacareli <leticia_careli@hotmail.com>
2023-02-02 08:40:44 -0300

    Aula 03
    Concatenação

---
+++ JAVASCRIPT_BASICO/03/app.js
@@ -0,0 +1,13 @@
+var nome = 'Letícia';
+console.log(nome);
+
+nome = 123;
+console.log(nome);
+
+var car = {
+    nome: "Fusion",
+    ano: 2018,
+    marca: 'Ford'
+};
+
+console.log('O ' + car.nome + ' é do ano de ' + car.ano + ' e da marca ' + car.marca); 
