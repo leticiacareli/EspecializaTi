ff34a2254317989d0d15db89cde447e8ede2ef7a
leticiacareli <leticia_careli@hotmail.com>
2023-02-03 14:38:50 -0300

    Incremento e decremento

---
+++ JAVASCRIPT_BASICO/02_operadores_aritmeticos/03/app.js
@@ -0,0 +1,20 @@
+var n1 = 12;
+var n2 = 4;
+
+console.log('n1 = ' + n1)
+
+//add antes
+var pre_incremento = ++n1;
+console.log('pre_incremento = ' + pre_incremento);
+
+//add depois
+var pos_incremento = n1++;
+console.log('pos_incremento = ' + n1);
+
+console.log('n2 = ' + n2)
+
+var pre_decremento = --n2;
+console.log('pre_decremento = ' + pre_decremento);
+
+var pos_decremento = n2--;
+console.log('pos_decremento = ' + n2);
