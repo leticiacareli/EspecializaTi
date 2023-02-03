36354d69f892cec62629b654e8b6bb490cda7e97
leticiacareli <leticia_careli@hotmail.com>
2023-02-03 14:55:11 -0300

    Operadores de atribuição

---
+++ JAVASCRIPT_BASICO/02_operadores_aritmeticos/05/app.js
@@ -0,0 +1,15 @@
+var n1 = 15;
+
+console.log('n1 = ' + n1);
+
+n1 += 1;
+console.log('+= 1 = ' + n1);
+
+n1 -= 2;
+console.log('-= 2 = ' + n1);
+
+n1 *= 3;
+console.log('*= 3 = ' + n1);
+
+n1 /= 4;
+console.log('/= 4 = ' + n1);
