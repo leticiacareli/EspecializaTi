2f0605d1b03e3f06bc1ab0e2755854e25605906e
leticiacareli <leticia_careli@hotmail.com>
2023-02-03 14:06:38 -0300

    Operadores Aritméticos
    Introdução aos operadores aritméticos

---
+++ JAVASCRIPT_BASICO/02_operadores_aritmeticos/01/app.js
@@ -0,0 +1,15 @@
+var n1 = 12;
+var n2 = 4;
+
+var somar = n1 + n2;
+var subtrair = n1 - n2;
+var multiplicar = n1 * n2;
+var dividir = n1 / n2;
+var potencia = n1 ** n2;
+
+console.log(n1 + ' + ' + n2 + ' = ' + somar);
+console.log(n1 + ' - ' + n2 + ' = ' + subtrair);
+console.log(n1 + ' x ' + n2 + ' = ' + multiplicar);
+console.log(n1 + ' / ' + n2 + ' = ' + dividir);
+console.log(n1 + ' ** ' + n2 + ' = ' + potencia);
+
