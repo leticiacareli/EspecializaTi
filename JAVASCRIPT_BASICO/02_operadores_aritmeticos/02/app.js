e11d27d14bfa5d75c8a4b86343421d737ccf7af5
leticiacareli <leticia_careli@hotmail.com>
2023-02-03 14:22:08 -0300

    Prioridade
    Ordem de prioridade dos operadores aritméticos

---
+++ JAVASCRIPT_BASICO/02_operadores_aritmeticos/02/app.js
@@ -0,0 +1,17 @@
+/*
+
+ordem de prioridade numa operação matemática
+
+ 1 -> raizes e potenciação;
+ 2 -> multiplicação e divisão;
+ 3 -> adição e subtração
+
+ 1 -> parênteses ();
+ 2 -> colchetes [];
+ 3 -> chaves {};
+
+ */
+
+var expressao = [(18 + 3 * 2) / 8 + 5 * 3] / 3;
+
+console.log(expressao);
