d4a1d674798d36777493b031e3033c9f0caa5a1d
leticiacareli <leticia_careli@hotmail.com>
2023-02-06 16:38:06 -0300

    Switch

---
+++ JAVASCRIPT_BASICO/03_operadores_condicionais/03/app.js
@@ -0,0 +1,14 @@
+var n = 1;
+var result = '';
+
+switch (n) 
+{
+    case 1:     
+        result = 'Um';        
+        break;
+    default:
+        result = 'Opção inválida!';
+        break;
+}
+
+console.log(result);
