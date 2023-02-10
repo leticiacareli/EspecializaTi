ab839ab10cf5014323da8661bf263ea9722e07a5
leticiacareli <leticia_careli@hotmail.com>
2023-02-10 08:53:54 -0300

    Introdução a arrays

---
+++ JAVASCRIPT_BASICO/05_arrays/01/app.js
@@ -0,0 +1,9 @@
+// Definir arrays
+
+var dogs = ['Lara', 'Canela', 'Mia', 'Babi'];
+console.log(dogs);
+console.log(dogs.length);
+
+var brothers = new Array('Jakson', 'Jefferson', 'Mia');
+console.log(brothers);
+console.log(brothers.length);
