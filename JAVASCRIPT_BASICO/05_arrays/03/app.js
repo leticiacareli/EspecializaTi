648068482592e95c5020c917441645223f6cfe02
leticiacareli <leticia_careli@hotmail.com>
2023-02-10 10:05:20 -0300

    Join, reverse, typeof, sort

---
+++ JAVASCRIPT_BASICO/05_arrays/03/app.js
@@ -0,0 +1,20 @@
+var family = [
+    'Lucia',
+    'Márcio',
+    'Letícia',
+    'Jefferson',
+    'Jakson',
+    'Mia',
+]
+
+// junta os itens do array, tendo a possibilidade de colocar um separador.
+console.log(family.join(' - '));
+
+// mostra o tipo
+console.log(typeof(family));
+
+// mostra o array de trás para frente
+console.log(family.reverse());
+
+// ordena os itens do array
+console.log(family.sort());
