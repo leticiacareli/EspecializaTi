af24936dc79686783674a61e42723355ecf05503
leticiacareli <leticia_careli@hotmail.com>
2023-02-10 10:36:17 -0300

    concat, slice, toString

---
+++ JAVASCRIPT_BASICO/05_arrays/05/app.js
@@ -0,0 +1,25 @@
+var humans = [
+    'Lucia',
+    'Márcio',
+    'Letícia',
+    'Jefferson',
+    'Jakson',
+    'Luan',
+    'Paulo',
+]
+
+var dogs = [
+    'Canela',
+    'Lara',
+    'Babi',
+    'Mia',
+]
+
+// concatenação de dois arrays
+console.log(humans.sort().concat(dogs.sort()));
+
+// mostra os itens a partir de um início e fim determindos.
+console.log(humans.slice(4, 6));
+
+// conversão para string
+console.log(humans.toString());
