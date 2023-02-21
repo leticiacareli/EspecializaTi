40405fcd64b79a01fd8dfdee3c9d564e5131a4b9
leticiacareli <leticia_careli@hotmail.com>
2023-02-21 12:14:54 -0300

    removendo produto do carrinho

---
+++ intro-vue.js/06/app.js
@@ -0,0 +1,41 @@
+const app = {
+    data() {
+        return {
+            title: 'Curso de Vue.js 3',
+            products: [
+                {
+                    title: 'Naruto',
+                    description: 'Anime do Naruto',
+                    image: './assets/img/naruto.jpg',
+                    stars: 0,
+                },
+                {
+                    title: 'All-Animes',
+                    description: 'Todos os animes',
+                    image: './assets/img/all-anime.jpg',
+                    stars: 5,
+                },
+            ],
+            cart: [],
+        }
+    },
+
+    methods:{
+        addCart(product){
+            this.cart.push(product);
+        },
+
+        // se o indexOf retornar diferente de -1, quer dizer o produto está no carrinho
+        inCart(product){
+            return this.cart.indexOf(product) != -1;
+        },
+
+        removeCart(product){
+            this.cart = this.cart.filter((prod) => {
+                return product != prod;
+            })
+        },
+    }
+}
+
+Vue.createApp(app).mount('#app')
